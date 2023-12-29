f9e118742d6103b2e8185456ae4ca39484e708da
Zane Koch <zanehkoch@gmail.com>
2023-12-29 08:15:15 -0800

    partially working datadownloader via TCGABIolinks

--- tcga/README.md
+++ tcga/README.md
@@ -4,8 +4,13 @@ Downloaded TCGABIolinks and associated packages to local RStudio environemnt
     BiocManager::install("TCGAbiolinks")
     install.packages('DT')
     BiocManager::install("sesameData")
+    BiocManager::install("sesame")
+    install.packages("arrow")
     
 Then used TCGABiolinks documentation ("https://bioconductor.org/packages/release/bioc/html/TCGAbiolinks.html")
+- This page (https://bioconductor.org/packages/release/bioc/vignettes/TCGAbiolinks/inst/doc/download_prepare.html) has code for downloading each data type
+  - the top of the page describes how to use colData, assay, and rowRanges to use the data that is downloaded (which comes in a summarizedData object)
+- This page (https://bioconductor.org/packages/release/bioc/vignettes/TCGAbiolinks/inst/doc/query.html) has the different value options for the arguments of query and other fxns
 
 
 Went to tcga website (with filter: https://portal.gdc.cancer.gov/repository?facetTab=cases&filters=%7B%22op%22%3A%22and%22%2C%22content%22%3A%5B%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22cases.samples.tissue_type%22%2C%22value%22%3A%5B%22normal%22%5D%7D%7D%2C%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22files.access%22%2C%22value%22%3A%5B%22open%22%5D%7D%7D%2C%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22files.data_category%22%2C%22value%22%3A%5B%22dna%20methylation%22%2C%22simple%20nucleotide%20variation%22%2C%22transcriptome%20profiling%22%5D%7D%7D%5D%7D&searchTableTab=cases)
--- tcga/TCGA_data_downloader.R
+++ tcga/TCGA_data_downloader.R
@@ -1,13 +1,84 @@
 library(TCGAbiolinks)
 library(DT)
+library(SummarizedExperiment)
+
+output_dir <- "/Users/zanekoch/Documents/ucsd/dream_proj/tcga/GDCdata"
+
+datasets <- c("TCGA-BRCA", "TCGA-COAD")
+data_categories <- c("Transcriptome Profiling", "DNA Methlation", "Simple Nucleotide Variation")
+data_types  <- c("Gene Expression Quantification", "Methylation Beta Value", "Masked Somatic Mutation")
+normal_sample_types <- c("Blood Derived Normal", "Solid Tissue Normal", "Buccal Cell Normal", "EBV Immortalized Normal", "Bone Marrow Normal")
+
+# if methylation then platform = "Illumina Human Methylation 450" or "Illumina Methylation Epic"
 
 
+# Loop through datasets, data categories, and data types
+for (dataset in datasets) {
+  for (category in data_categories) {
+    # select dataypes
+    if (category == "Transcriptome Profiling") {
+      dtype <- data_types[1]
+    } else if (category == "DNA Methlation") {
+      dtype <- data_types[2]
+      #platform <- c("Illumina Human Methylation 450", "Illumina Methylation Epic")
+    } else { # simple nucleotide variation
+      dtype <- data_types[3]
+    }
+    # Download data
+    query <- GDCquery(project = dataset, 
+                      data.category = category, 
+                      data.type = dtype,
+                      sample.type = normal_sample_types,
+                      )
+    
+    GDCdownload(query, directory = output_dir)
+    
+    # Load the data
+    data <- GDCprepare(query, directory = output_dir)
+    
+    # Display some information about the downloaded data
+    cat("Dataset:", dataset, "\n")
+    cat("Data Category:", category, "\n")
+    cat("Data Type:", dtype, "\n")
+    cat("Number of samples:", ncol(colData(data)), "\n")
+    cat("\n")
+  }
+}
+
+
+
+
+
+query <- GDCquery(project = "TCGA-BRCA", 
+                  data.category = "Transcriptome Profiling", 
+                  data.type = "Gene Expression Quantification", 
+                  experimental.strategy = "RNA-Seq", 
+                  sample.type = "Solid Tissue Normal")
+query$results
+
+# choose the samples to download
 query_met.hg38 <- GDCquery(
-  project= "TCGA-LGG", 
-  data.category = "DNA Methylation", 
-  data.type = "Methylation Beta Value",
-  platform = "Illumina Human Methylation 450", 
-  barcode = c("TCGA-HT-8111-01A-11D-2399-05","TCGA-HT-A5R5-01A-11D-A28N-05")
+  project= "CPTAC-3", 
+  data.category = "Transcriptome Profiling", 
+  #data.type = "Methylation Beta Value",
+  #platform = "Illumina Human Methylation 450", 
+  #barcode = c("TCGA-HT-8111-01A-11D-2399-05","TCGA-HT-A5R5-01A-11D-A28N-05"),
+  #sample.type = "Solid Tissue Normal"
+  #sample.type = c("NB", "NT", "NBC", "NEBV", "NBM")
+  sample.type = c("Blood Derived Normal", "Solid Tissue Normal", "Buccal Cell Normal", "EBV Immortalized Normal", "Bone Marrow Normal")
 )
-GDCdownload(query_met.hg38)
-data.hg38 <- GDCprepare(query_met.hg38)
+query_met.hg38$results
+
+# download them to GDCdata dir
+GDCdownload(query_met.hg38, directory = "/Users/zanekoch/Documents/ucsd/dream_proj/tcga/GDCdata")
+# can  set summarizedExperiment to FALSE to get a df
+data.hg38 <- GDCprepare(
+  query_met.hg38, summarizedExperiment = FALSE,
+  directory = "/Users/zanekoch/Documents/ucsd/dream_proj/tcga/GDCdata"
+  )
+
+colData(data.hg38)
+# data matrix
+assay(data.hg38)
+# not sure what this is 
+rowRanges(data.hg38)
