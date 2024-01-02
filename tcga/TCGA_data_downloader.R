d9144aecac5fc3259103393a7f187384509af7a9
zanekoch <zanehkoch@gmail.com>
2024-01-01 16:21:16 -0800

    adding environments

--- tcga/TCGA_data_downloader.R
+++ tcga/TCGA_data_downloader.R
@@ -1,99 +1,53 @@
 library(TCGAbiolinks)
 library(DT)
 library(SummarizedExperiment)
+# set download timeout limit to 100 minutes
+options(timeout = 6000)
 
-output_dir <- "/Users/zanekoch/Documents/ucsd/dream_proj/tcga/GDCdata"
-
-datasets <- c("TCGA-COAD", "TCGA-BRCA")
-data_categories <- c("DNA Methylation","Transcriptome Profiling",  "Simple Nucleotide Variation")
-normal_sample_types <- c("Blood Derived Normal", "Solid Tissue Normal", "Buccal Cell Normal", "EBV Immortalized Normal", "Bone Marrow Normal")
-
-# if methylation then platform = "Illumina Human Methylation 450" or "Illumina Methylation Epic"
+#datasets <- c("TCGA-COAD", "TCGA-BRCA")
+#data_categories <- c("DNA Methylation","Transcriptome Profiling",  "Simple Nucleotide Variation")
+#normal_sample_types <- c("Blood Derived Normal", "Solid Tissue Normal", "Buccal Cell Normal", "EBV Immortalized Normal", "Bone Marrow Normal")
 
 
-# Loop through datasets, data categories, and data types
-for (dataset in datasets) {
-  for (category in data_categories) {
-    # select dataypes
-    if (category == "Transcriptome Profiling") {
-      dtype <- "Gene Expression Quantification"
-      # query
-      query <- GDCquery(project = dataset, 
-                        data.category = category, 
-                        data.type = dtype,
-                        sample.type = normal_sample_types,
-                        )
-    } else if (category == "DNA Methylation") {
-      dtype <- "Methylation Beta Value"
-      # TCGA is all 450k/27k but we are only going to use 450k
-      if (startsWith(dataset, "TCGA")) {
-        platform <- "Illumina Human Methylation 450"
-      # while CPTAC and someothers are Epic
-      } else {
-        platform <- "Illumina Methylation Epic"
-      }
-      # query with platform
-      query <- GDCquery(project = dataset, 
-                        data.category = category, 
-                        data.type = dtype,
-                        sample.type = normal_sample_types,
-                        platform = platform
-                        )
-    } else { # simple nucleotide variation
-      dtype <- "Masked Somatic Mutation"
-      # query
-      query <- GDCquery(project = dataset, 
-                        data.category = category, 
-                        data.type = dtype,
-                        sample.type = normal_sample_types,
-                        )
-    }
-    
-    GDCdownload(query, directory = output_dir)
-    
-    # Load the data
-    data <- GDCprepare(query, directory = output_dir)
-    
-    # Display some information about the downloaded data
-    cat("Dataset:", dataset, "\n")
-    cat("Data Category:", category, "\n")
-    cat("Data Type:", dtype, "\n")
-    cat("Number of samples:", ncol(colData(data)), "\n")
-    cat("\n")
-  }
-}
 
-
-# CPTAC-3 mutations
-query <- GDCquery(
-  project = "CPTAC-3",
+# write a function that takes in a project and returns the samples with both mutation and expression data
+# and then download the data for those samples
+#get_mut_expr_samples <- function(project, output_dir="/cellar/users/zkoch/dream/tcga/GDCdata") {
+  # get mutation data
+output_dir<-"/cellar/users/zkoch/dream/tcga/GDCdata"
+project <- "CPTAC-3"
+query_mut <- GDCquery(
+  project = project,
   data.category = "Simple Nucleotide Variation",
   data.type = "Masked Somatic Mutation", 
-  #workflow.type = "STAR - Counts",
-  #barcode = c("TCGA-14-0736-02A-01R-2005-01", "TCGA-06-0211-02A-02R-2005-01")
+  access = "open"
 )
-View(getResults(query))
-
-table(getResults(query)$sample_type)
-
-# CPTAC-3 expr
-query1 <- GDCquery(
-  project = "CPTAC-3",
+# get expression data
+query_expr <- GDCquery(
+  project = project,
   data.category = "Transcriptome Profiling",
   data.type = "Gene Expression Quantification", 
-  #workflow.type = "STAR - Counts",
-  #barcode = c("TCGA-14-0736-02A-01R-2005-01", "TCGA-06-0211-02A-02R-2005-01")
+  workflow.type = "STAR - Counts",
+  access = "open",
 )
-table(getResults(query1)$sample_type)
-
-# CPTAC-3 methyl
-query2 <- GDCquery(
-  project = "CPTAC-3",
-  data.category = "DNA Methylation",
-  data.type = "Methylation Beta Value", 
-  #workflow.type = "STAR - Counts",
-  #barcode = c("TCGA-14-0736-02A-01R-2005-01", "TCGA-06-0211-02A-02R-2005-01")
+# print how many samples of each type
+print(paste("Mutation samples:", length(getResults(query_mut, cols = "cases"))))
+print(paste("Expression samples:", length(getResults(query_expr, cols = "cases"))))
+shared_samples <- intersect(
+    substr(getResults(query_mut, cols = "cases"), 1, 12),
+    substr(getResults(query_expr, cols = "cases"), 1, 12)
 )
-table(getResults(query2)$sample_type)
+print(paste("Samples with both mutation and expression data:", length(shared_samples)))
 
+GDCdownload(query_mut, directory = output_dir)
+GDCdownload(query_expr, directory = output_dir)
+print("Downloaded data")
+# Load the data as a SummarizedExperiment
+data_mut <- GDCprepare(query_mut, directory = output_dir, summarizedExperiment = TRUE)
+data_expr <- GDCprepare(query_expr, directory = output_dir, summarizedExperiment = TRUE)
+# return the data
+return(list(data_mut, data_expr))
+  #}
 
+# run the function
+data <- get_mut_expr_samples("CPTAC-3")
